3e7aa5a6071636529c678b4c619e500c08d2c0ec
Shams425 <alexdavid486@gmail.com>
2022-07-07 10:18:35 +0200

    styling the form

--- send-mail/index.php
+++ send-mail/index.php
@@ -16,25 +16,24 @@
 <body>
     <div class="container">
       <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-6 col-sm-12 mail-form form-control">
           <div class="header mt-5">
             <h2 class="text-center">Send Message</h2> 
             <p class="text-center">send mail to anyone from localhost</p>
           </div>
-
           <main>
             <form action="#" method="POST" autocomplete="off" class="mt-5">
               <div class="form-group mb-3">
-                <input type="email" class="form-control p-3" placeholder="you e-mail">
+                <input type="email" class="form-control" placeholder="you e-mail">
               </div>
               <div class="form-group mb-3">
-                <input type="text" class="form-control p-3" placeholder="Subject">
+                <input type="text" class="form-control" placeholder="Subject">
               </div>
               <div class="form-group mb-3">
-                <textarea name="message" id="" cols="30" rows="10" class="form-control p-3" placeholder="Your message"></textarea>
+                <textarea name="message" id="" cols="30" rows="10" class="form-control textarea" placeholder="Your message"></textarea>
               </div>
               <div class="form-group mb-3">
-                <input type="submit" class="form-control p-3 btn btn-primary" value="Send">
+                <input type="submit" class="form-control btn btn-primary" value="Send">
               </div>
             </form>
           </main>

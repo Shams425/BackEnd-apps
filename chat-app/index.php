24aa2cecec86b027fcf02cf3df50ee763132621c
Shams425 <alexdavid486@gmail.com>
2022-07-10 08:46:21 +0200

    starting new project

---
+++ chat-app/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Chat app</title>
+
+  <!-- font awesome library -->
+  <link rel="stylesheet" href="assets/css/all.min.css">
+
+  <!-- main css file -->
+  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+  
+</body>
+</html>

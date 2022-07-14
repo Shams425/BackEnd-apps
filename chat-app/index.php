d919a06cbb114ef204261d858e0b107d83edb39d
Shams425 <alexdavid486@gmail.com>
2022-07-14 07:24:47 +0200

    making the pages daynamic

--- chat-app/index.php
+++ chat-app/index.php
@@ -1,135 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Chat app</title>
 
-    <!-- font awesome library -->
-    <link rel="stylesheet" href="assets/css/all.min.css" />
-    <!-- bootstrap library -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Chat app</title>
 
-    <!-- google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
+  <!-- font awesome library -->
+  <link rel="stylesheet" href="./assets/css/all.min.css" />
+  <!-- bootstrap library -->
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
 
-    <!-- main css file -->
-    <link rel="stylesheet" href="assets/css/main.css" />
-  </head>
-  <body>
-    
-    <div class="container">
-      <div class="row">
-        <section class="contentContainer sign-up">
-          <div class="wrapper">
-            <div class="title">
-              <h1>Realtime Chat app</h1>
-            </div>
-            <form action="#" enctype="multipart/form-data" method="POST">
-              <!-- signup status messages -->
-              <div class="alert alert-danger mt-3 text-center">
-                Error Message
-              </div>
-              <div class="alert alert-info mt-3 text-center">Error Message</div>
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
+    rel="stylesheet" />
 
-              <!-- form data -->
-              <div class="nameDetails d-flex flex-wrap">
-                <div class="firstName col-lg-6 col-md-6 col-sm-6">
-                  <div class="form-group">
-                    <label for="text" class="mb-2">First Name</label>
-                    <input
-                      type="text"
-                      name="firstName"
-                      required="true"
-                      class="form-control"
-                      placeholder="Your First Name"
-                    />
-                  </div>
-                </div>
-                <div class="lastName col-lg-6 col-md-6 col-sm-6">
-                  <div class="form-group mb-3">
-                    <label for="text" class="mb-2">Last Name</label>
-                    <input
-                      type="text"
-                      name="lastName"
-                      required="true"
-                      class="form-control"
-                      placeholder="Your Last Name"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="email form-group mb-3">
-                <label for="email" class="mb-2">Email Address</label>
-                <input
-                  type="email"
-                  name="email"
-                  required="true"
-                  class="form-control"
-                  placeholder="Enter You E-mail"
-                />
-              </div>
-              <div class="password form-group mb-3">
-                <label for="password" class="mb-2">Password</label>
-                <input
-                  type="password"
-                  name="password"
-                  required="true"
-                  class="form-control"
-                  placeholder="Enter Your Password"
-                />
-                <i class="fas fa-eye"></i>
-              </div>
-              <div class="password form-group mb-3">
-                <label for="password" class="mb-2">Rewrite Password</label>
-                <input
-                  type="password"
-                  name="rePassword"
-                  required="true"
-                  class="form-control"
-                  placeholder="Rewrite Your Password"
-                />
-                <i class="fas fa-eye"></i>
-              </div>
-              <div class="image form-group mb-3">
-                <label for="file" class="mb-2">Select Profile Img</label>
-                <input
-                  type="file"
-                  name="image"
-                  required="true"
-                  class="form-control"
-                />
-              </div>
-              <div class="form-group mt-5 text-center button">
-                <input
-                  type="submit"
-                  name="send"
-                  class="btn btn-dark"
-                  value="Start Chatting"
-                />
-              </div>
-            </form>
+  <!-- main css file -->
+  <link rel="stylesheet" href="./assets/css/main.css" />
+</head>
 
-            <!-- prevues user -->
-            <div class="link text-center mt-3">
-              Already signed up ?
-              <a href="#">Login now</a>
-            </div>
+<body>
+  <div class="container">
+    <div class="row">
+      <section class="contentContainer sign-up">
+        <div class="wrapper">
+          <div class="title">
+            <h1>Realtime Chat app</h1>
           </div>
-        </section>
-      </div>
-    </div>
+          <form action="#" enctype="multipart/form-data" method="POST">
+            <!-- signup status messages -->
+            <div class="alert alert-danger mt-3 text-center"></div>
 
-    <!-- external library -->
-    <script src="assets/js/bootstrap.min.js"></script>
-    <!-- main js file -->
-    <script src="assets/js/show-hide-password.js"></script>
-    <script src="assets/js/signup.js"></script>
-  </body>
+            <!-- form data -->
+            <div class="nameDetails d-flex flex-wrap">
+              <div class="firstName col-lg-6 col-md-6 col-sm-6">
+                <div class="form-group">
+                  <label for="text" class="mb-2">First Name</label>
+                  <input type="text" name="firstName" required="true" class="form-control"
+                    placeholder="Your First Name" />
+                </div>
+              </div>
+              <div class="lastName col-lg-6 col-md-6 col-sm-6">
+                <div class="form-group mb-3">
+                  <label for="text" class="mb-2">Last Name</label>
+                  <input type="text" name="lastName" required="true" class="form-control"
+                    placeholder="Your Last Name" />
+                </div>
+              </div>
+            </div>
+            <div class="email form-group mb-3">
+              <label for="email" class="mb-2">Email Address</label>
+              <input type="email" name="email" required="true" class="form-control" placeholder="Enter You E-mail" />
+            </div>
+            <div class="password form-group mb-3">
+              <label for="password" class="mb-2">Password</label>
+              <input type="password" name="password" required="true" class="form-control"
+                placeholder="Enter Your Password" />
+              <i class="fas fa-eye"></i>
+            </div>
+            <div class="password form-group mb-3">
+              <label for="password" class="mb-2">Rewrite Password</label>
+              <input type="password" name="rePassword" required="true" class="form-control"
+                placeholder="Rewrite Your Password" />
+              <i class="fas fa-eye"></i>
+            </div>
+            <div class="image form-group mb-3">
+              <label for="file" class="mb-2">Select Profile Img</label>
+              <input type="file" name="image" required="true" class="form-control" />
+            </div>
+            <div class="form-group mt-5 text-center button">
+              <input type="submit" name="send" class="btn btn-dark" value="Start Chatting" />
+            </div>
+          </form>
+
+          <!-- prevues user -->
+          <div class="link text-center mt-3">
+            Already signed up ?
+            <a href="pages/login.php">Login now</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  </div>
+
+  <!-- external library -->
+  <script src="./assets/js/bootstrap.min.js"></script>
+  <!-- main js file -->
+  <script src="./assets/js/show-hide-password.js"></script>
+  <script src="./assets/js/signup.js"></script>
+</body>
+
 </html>
--- chat-app/pages/users.php
+++ chat-app/pages/users.php
@@ -1,165 +1,66 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Chat app</title>
+<?php
+  session_start();
+  if (!isset($_SESSION['unique_id'])) {
+      header('location: login.php');
+  }
+  ?>
 
-    <!-- font awesome library -->
-    <link rel="stylesheet" href="../assets/css/all.min.css" />
-    <!-- bootstrap library -->
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
+<?php include_once "../assets/php/header.php"; ?>
 
-    <!-- google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
+<body>
+  <div class="container">
+    <div class="row">
+      <section class="contentContainer users">
+        <div class="wrapper">
+          <header>
+            <!-- include the database -->
+            <?php
+  include_once "../assets/php/config.php";
+  $sql = mysqli_query($data_base, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
 
-    <!-- main css file -->
-    <link rel="stylesheet" href="../assets/css/main.css" />
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <section class="contentContainer users">
-          <div class="wrapper">
-            <header>
-              <div class="content">
-                <!-- profile picture -->
-                <img src="../assets/images/profile-4.jpg" alt="" />
+  if (mysqli_num_rows($sql) > 0) {
+      $row = mysqli_fetch_assoc($sql);
+  }
+  ?>
 
-                <div class="details">
-                  <span>Chatting room</span>
-                  <p>Active now</p>
-                </div>
+            <div class="content">
+              <!-- profile picture -->
+              <img
+                src="../assets/php/images/<?php echo "{$row['img']}"; ?>"
+                alt="" />
+
+              <div class="details">
+                <span><?php echo "{$row['first_name']}". " " . "{$row['last_name']}"; ?></span>
+                <p><?php echo "{$row['status']}"?>
+                </p>
               </div>
-
-              <!-- logout -->
-              <a href="#" class="logout btn btn-dark">Logout</a>
-            </header>
-
-            <!-- search for chat -->
-            <div class="search">
-              <span class="text">Select user to start chatting</span>
-              <input
-                type="text"
-                name="search"
-                id=""
-                class="form-control"
-                placeholder="Enter user name ..."
-              />
-              <button class="btn btn-outline-dark">
-                <i class="fas fa-search"></i>
-              </button>
             </div>
 
-            <!-- list of users -->
-            <div class="usersList">
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
+            <!-- logout -->
+            <a href="#" class="logout btn btn-dark">Logout</a>
+          </header>
 
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
-
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
-
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
-
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
-
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
-
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-              <a href="">
-                <div class="content">
-                  <img src="../assets/images/profile-6.jpg" alt="" />
-
-                  <div class="details">
-                    <span>User 1</span>
-                    <p>Message for testing</p>
-                  </div>
-                </div>
-                <div class="showStatus">
-                  <i class="fas fa-circle"></i>
-                </div>
-              </a>
-            </div>
+          <!-- search for chat -->
+          <div class="search">
+            <span class="text">Select user to start chatting</span>
+            <input type="text" name="search" id="" class="form-control" placeholder="Enter user name ..." />
+            <button class="btn btn-outline-dark">
+              <i class="fas fa-search"></i>
+            </button>
           </div>
-        </section>
-      </div>
+
+          <!-- list of users -->
+          <div class="usersList"></div>
+        </div>
+      </section>
     </div>
+  </div>
 
-    <!-- external library -->
-    <script src="../assets/js/bootstrap.min.js"></script>
+  <!-- external library -->
+  <script src="../assets/js/bootstrap.min.js"></script>
 
-    <!-- main js file -->
-    <script src="../assets/js/"></script>
-  </body>
+  <!-- main js file -->
+  <script src="../assets/js/users.js"></script>
+</body>
+
 </html>

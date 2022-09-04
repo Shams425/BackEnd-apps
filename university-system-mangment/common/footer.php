4069014bca8c856b9c1418a1cad2257f1b140e81
Shams425 <alexdavid486@gmail.com>
2022-09-04 16:27:46 +0200

    starting new project making the main page, config file, common files

---
+++ university-system-mangment/common/footer.php
@@ -0,0 +1,29 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">BS Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: Binalbagan,
+                  Negros Occidental – Philippines</p>
+              <p>Telephones: +639272777334</p>
+              <p>E-mail: suarez081119@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © Alright Reserve 2022</p>
+</footer>
+<script src="bootstrap/js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
---
+++ university-system-mangment/config/config.php
@@ -0,0 +1,6 @@
+<?php 
+	$con = mysqli_connect("localhost", "root","", "college");
+	if(!$con) {
+		echo "Something went wrong";
+	}
+?>

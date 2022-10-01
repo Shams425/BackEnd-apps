9f8164318db016df4cc370ca3eaf4f0c58146929
Shams425 <alexdavid486@gmail.com>
2022-10-01 11:55:45 +0200

    updating the nav bar header of the control panle

--- university-system-mangment/common/header.php
+++ university-system-mangment/common/header.php
@@ -13,28 +13,66 @@
 		<link rel="stylesheet" href="./assets/css/main.css">
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-dark header-back sticky-top header-navbar-fonts">
-			<a class="navbar-brand d-flex align-items-center" href="index.php">
-				<img src="images/chmsc.png" class="logo-image" width="50" height="50">
-				<h3 class="text-light text-uppercase ml-2">CHMSC</h3>
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav ml-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="index.php">HOME<span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item active">
-						<a class="nav-link" href="academics.php">ACADEMICS<span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item active">
-						<a class="nav-link" href="admission.php">ADMISSION<span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item active">
-						<a class="nav-link" href="login/login.php"><i class="fa fa-sign-in text-white fa-lg" aria-hidden="true"></i><span class="sr-only">(current)</span></a>
-					</li>
-				</ul>
-			</div>
-	</nav>
+		<header>
+			<nav class="navbar navbar-expand-sm navbar-light bg-light">
+				<div class="container d-flex align-items-center">
+					<div class="sch-info d-flex align-items-center flex-wrap">
+						<a class="navbar-brand" href="#">
+							<i class="nav-icon fas fa-pen"></i>
+						</a>
+						<!-- <span>Welcome to X-Code school management Dashboard</span> -->
+					</div>
+
+					<div class="nav-shortCuts d-flex align-items-center flex-wrap">
+						<button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+							aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="collapsibleNavId">
+							<ul class="navbar-nav me-auto mt-2 mt-lg-0">
+								<li class="nav-item">
+									 <form action="" method="post">
+										<input type="text" class="form-control" placeholder="Search ...">
+										<i class="fas fa-search"></i>
+									 </form>
+								</li>
+								<li class="nav-item dropdown">
+									<select name="Languages" id="" class="form-control">
+										<option value="English">English</option>
+										<option value="Arabic">Arabic</option>
+									</select>
+								</li>
+								<li class="nav-item">
+									<div class="notifications d-flex align-items-center">
+										<div class="icon">
+											<i class="fas fa-envelope"></i>
+											<span class="active">3</span>
+										</div>
+										<div class="icon">
+											<i class="fas fa-bell mx-3"></i>
+											<span class="active">12</span>
+										</div>
+									</div>
+								</li>
+								<li class="nav-item profile">
+									<div class="user d-flex align-items-center">
+										<div class="profilePic">
+											<!-- <img src="./assets/img/" alt="" width="50"> -->
+											<i class="fas fa-user mx-3"></i>
+										</div>
+										<select name="userSelect" id="" class="form-control">
+											<option value="Ahmed">Ahmed Yassin</option>
+											<option value="Mohamed"></option>
+											<option value="Ibrahim"></option>
+										</select>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</nav>
+			
+			
+		</header>
+		

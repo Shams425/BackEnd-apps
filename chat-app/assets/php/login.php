7c3a0a7ac81805d7925753e2576f31696dc39171
Shams425 <alexdavid486@gmail.com>
2022-10-03 21:34:02 +0200

    organize the code

--- chat-app/assets/php/login.php
+++ chat-app/assets/php/login.php
@@ -7,32 +7,32 @@ $password  = mysqli_real_escape_string($data_base, $_POST['password']);
 
 
 if (!empty($email) && !empty($password)) {
-    $sql = mysqli_query($data_base, "SELECT * FROM users 
-    WHERE 
-      email = '$email' 
-    AND 
-      password = '$password'");
+  $sql = mysqli_query($data_base, "SELECT * FROM users 
+  WHERE 
+    email = '$email' 
+  AND 
+    password = '$password'");
 
-    if (mysqli_num_rows($sql) > 0) {
-        $row = mysqli_fetch_assoc($sql);
-        $status = "active";
-        
-        //updating the user status when login is successful
-        $sql2 = mysqli_query(
-            $data_base,
-            "UPDATE users 
-            SET 
-              status = '$status' 
-            WHERE 
-              unique_id = {$row['unique_id']}"
-        );
-        if ($sql2) {
-            $_SESSION['unique_id'] = $row['unique_id']; //this session is used by other php files
-            echo"success";
-        }
-    } else {
-        echo "email or password is incorrect";
+  if (mysqli_num_rows($sql) > 0) {
+    $row = mysqli_fetch_assoc($sql);
+    $status = "active";
+    
+    //updating the user status when login is successful
+    $sql2 = mysqli_query(
+      $data_base,
+      "UPDATE users 
+      SET 
+        status = '$status' 
+      WHERE 
+        unique_id = {$row['unique_id']}"
+    );
+    if ($sql2) {
+      $_SESSION['unique_id'] = $row['unique_id']; //this session is used by other php files
+      echo"success";
     }
+  } else {
+    echo "email or password is incorrect";
+  }
 } else {
-    echo "all inputs are required";
+  echo "all inputs are required";
 }

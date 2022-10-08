2bef7ebfbf2a3ce4b37bbf9f57ad7fbb9dfe0061
Shams425 <alexdavid486@gmail.com>
2022-10-08 20:36:17 +0200

    addit title to the card

--- university-system-mangment/common/cards.php
+++ university-system-mangment/common/cards.php
@@ -1,6 +1,6 @@
 <div class="card">
   <div class="card-body">
-    <h4 class="card-title">Title</h4>
+    <h4 class="card-title">Total revenue</h4>
     <p class="card-text">Text</p>
   </div>
 </div>
